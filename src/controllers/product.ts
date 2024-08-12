038d1cb2a7fb6ead0fda2cc53767373e1ef7aee3
Utkarsh Shinde <utkarshshinde279@gmail.com>
2024-08-12 18:36:04 +0530

    error checks added for product and added adminonly middleware in api

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -3,6 +3,8 @@ import { TryCatch } from "../middlewares/error.js";
 
 import { NewProductRequest } from "../types/types.js";
 import { Product } from "../models/product.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { rm } from "fs";
 
 export const handleNewProduct = TryCatch(
   async (
@@ -12,7 +14,15 @@ export const handleNewProduct = TryCatch(
   ) => {
     const { name, price, stock, category, desc } = req.body;
     console.log("request body :: " + req.body.name);
-    const _photoPath = req.file;
+    const _photo = req.file;
+
+    if (!_photo) return next(new ErrorHandler("Please add photo", 400));
+
+    if (!name || !price || !stock || !category) {
+      rm(_photo.path, () => console.log("photo deleted"));
+
+      return next(new ErrorHandler("Please add all fields", 400));
+    }
 
     let _product = await Product.create({
       name,
@@ -20,7 +30,7 @@ export const handleNewProduct = TryCatch(
       stock,
       category: category?.toLowerCase(),
       desc,
-      photo: _photoPath?.path,
+      photo: _photo?.path,
     });
 
     return res.status(200).json({ success: true, _product });
